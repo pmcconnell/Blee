f2b55ff7dc57bbf4d087ae3093b7beee7305c777
Patrick McConnell <patrick@dogboystudios.com>
2013-04-06 18:29:39 -0400

    Initial Commit

---
+++ Blee/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Blee
+//
+//  Created by Patrick McConnell on 4/6/13.
+//  Copyright (c) 2013 Patrick McConnell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
